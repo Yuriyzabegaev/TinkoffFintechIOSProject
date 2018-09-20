43ced392b95bfe3e9ec9bdd73987631cf34784c7
Yuriy Zabegaev <zabegaev99@gmail.com>
2018-09-20 23:52:53 +0300

    Initial Commit

---
+++ TinkoffFintechIOSProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffFintechIOSProject
+//
+//  Created by Юрий Забегаев on 20.09.2018.
+//  Copyright © 2018 Юрий Забегаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
